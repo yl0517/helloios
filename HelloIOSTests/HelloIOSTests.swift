d3050a12a72137940589cb34fbca53fd63fabb52
yl0517 <yl0517@uw.edu>
2025-04-04 11:29:39 -0700

    Initial Commit

---
+++ HelloIOSTests/HelloIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSTests.swift
+//  HelloIOSTests
+//
+//  Created by Yoobin Lee on 4/4/25.
+//
+
+import Testing
+@testable import HelloIOS
+
+struct HelloIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
